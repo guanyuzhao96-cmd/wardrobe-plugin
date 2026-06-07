d73082b31bd1520dfbfc47922d6942e0e656da66
guanyuzhao96-cmd <guanyuzhao96@gmail.com>
2026-06-07 17:13:35 +0800

    fix: 4-column grid, narrower cards, smaller fonts

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // jsDelivr CDN 地址
-const CDN_URL = 'https://gcore.jsdelivr.net/gh/guanyuzhao96-cmd/wardrobe-plugin@610a171/wardrobe-plugin.js';
+const CDN_URL = 'https://gcore.jsdelivr.net/gh/guanyuzhao96-cmd/wardrobe-plugin@0d547c9/wardrobe-plugin.js';
 const content = "import '" + CDN_URL + "'";
 
 const config = {
